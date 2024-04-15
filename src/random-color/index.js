630bac64d3011742920d0dc6045ca2643d1cb590
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2024-04-16 01:07:35 +0200

    generate random color function

--- src/random-color/index.js
+++ src/random-color/index.js
@@ -3,8 +3,23 @@ import { useState } from "react";
 export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
-  function handleCreateHexColor() {}
-  function handleCreateRgbColor() {}
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+  function handleCreateHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  }
+  function handleCreateRgbColor() {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+    setColor(`rgb{r},{g},{b}`);
+  }
   return (
     <div
       style={{
@@ -23,6 +38,19 @@ export default function RandomColor() {
       >
         Generate Random Color
       </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "#fff",
+          fontSize: "60px",
+          marginTop: "50px",
+        }}
+      >
+        <h3>{typeOfColor === "rgb" ? "RGB Color" : "HEX Color"}</h3>
+        <h1>{color}</h1>
+      </div>
     </div>
   );
 }
